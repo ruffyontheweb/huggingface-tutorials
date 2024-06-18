2bdc75f37caa4c83353643f91ccba1ddd1f7d6b6
Ruffy Zarookian <ruffy@ruffyontheweb.com>
2024-06-18 02:20:12 +0000

    Clean up train.py and chapter 3 tutorial

--- train.py
+++ train.py
@@ -5,26 +5,26 @@ from datasets import load_dataset
 from tqdm.auto import tqdm
 
 
-
+#create iterator function for tokenizing the dataset
 def tokenize_function(example):
     return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
 
-
-
-
 accelerator = Accelerator()
 
+#load dataset, the checkpoint, and tokenize (from the checkpoint) and the dataCollator appropriate for this specific tokenizer
 raw_datasets = load_dataset("glue", "mrpc")
 checkpoint = "bert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
+#Run the "tokenize_function" over the dataset using the map function and return a pointer to the updated dataset on disk
 tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
-#format the data
+#format the data on dist via the tokenized_dataset object
 tokenized_datasets = tokenized_datasets.remove_columns(["sentence1", "sentence2", "idx"])
 tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
 tokenized_datasets.set_format("torch")
-tokenized_datasets["train"].column_names
+
+#create a data object for the training set and the validation set
 train_dataloader = DataLoader(
     tokenized_datasets["train"], shuffle=True, batch_size=8, collate_fn=data_collator
 )
@@ -32,17 +32,16 @@ eval_dataloader = DataLoader(
     tokenized_datasets["validation"], batch_size=8, collate_fn=data_collator
 )
 
+#creat the model and the training optimizer
 model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
 optimizer = AdamW(model.parameters(), lr=3e-5)
 
-train_dataloader = DataLoader(
-    tokenized_datasets["train"], shuffle=True, batch_size=8, collate_fn=data_collator
-)
-
+#create objects from the accelerate library to run
 train_dl, eval_dl, model, optimizer = accelerator.prepare(
     train_dataloader, eval_dataloader, model, optimizer
 )
 
+#Set up the training schedule
 num_epochs = 3
 num_training_steps = num_epochs * len(train_dl)
 lr_scheduler = get_scheduler(
@@ -52,8 +51,10 @@ lr_scheduler = get_scheduler(
     num_training_steps=num_training_steps,
 )
 
+#setup progress bar...
 progress_bar = tqdm(range(num_training_steps))
 
+#training call with the accelerate convention
 model.train()
 for epoch in range(num_epochs):
     for batch in train_dl:
